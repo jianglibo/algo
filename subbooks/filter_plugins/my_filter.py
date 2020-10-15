c2d52d16c97a6e01030881b77304a0458b606670
jianglibo <jianglibo@gmail.com>
2020-10-16 07:49:11 +0800

    custom filter.

---
+++ subbooks/filter_plugins/my_filter.py
@@ -0,0 +1,13 @@
+class FilterModule(object):
+    def filters(self):
+        return {
+            'a_filter': self.a_filter,
+            'another_filter': self.b_filter
+        }
+
+    def a_filter(self, a_variable):
+        a_new_variable = a_variable + ' CRAZY NEW FILTER'
+        return a_new_variable
+    def b_filter(self, a_variable):
+        a_new_variable = a_variable + ' CRAZY NEW FILTER'
+        return a_new_variable
